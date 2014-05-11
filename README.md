f89c7b98a00ecad231af48e7cf534de04036ab54
Chris <chris@digitalformula.net>
2014-05-12 01:06:59 +1000

    Project creation & initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# digitalformula/laravel
+
+PHP libraries for use with Laravel 4
+
+# Libraries available
+
+- Form validator based on input and an array of rules
---
+++ src/DigitalFormula/Laravel/Validator.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ * Contains the Validator class
+ *
+ * @author Chris Rasmussen, digitalformula.net
+ * @category Libraries & Utilities
+ * @license MIT
+ * @package DigitalFormula\Laravel
+ *
+ */
+
+namespace DigitalFormula\Laravel;
+
+/**
+ *
+ * The Validator class
+ *
+ * @package DigitalFormula\Laravel
+ *
+ */
+class Validator
+{
+    /**
+     * Validate a Laravel form
+     * @param array $rules The form rules to compare against
+     * @param array $input The form data to use as input
+     * @returns bool Whether or not the form is valid
+     */
+    public function ValidateForm( $rules, $input )
+    {
+        $validation = \Validator::make( $input, $rules );
+        return( $validation->fails() ? false : true );
+    }
+    /* ValidateForm */
+
+}
+/* Validator */
