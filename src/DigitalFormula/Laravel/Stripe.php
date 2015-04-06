f733b45eb8431bc68d6e678fcec20e12951f9d21
Chris Rasmussen <chris@digitalformula.net>
2015-04-06 22:06:39 +1200

    Fix static method error

--- src/DigitalFormula/Laravel/Stripe.php
+++ src/DigitalFormula/Laravel/Stripe.php
@@ -18,26 +18,12 @@ class Stripe
 {
 
 	/**
-	 * Information about the logged-in user
-	 *
-	 * @var
-	 */
-	var $user;
-
-	/**
-	 * Instantiate the class
-	 */
-	public function __construct()
-	{
-		$this->user = Sentinel::getUser();
-	}
-
-	/**
 	 * Get the logged-in user's Stripe ID
 	 */
-	public function myStripeId()
+	public static function myStripeId()
 	{
-		return $this->user->stripe_id;
+		$user = Sentinel::getUser();
+		return $user->stripe_id;
 	}
 
 }
